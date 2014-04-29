074cc1b711ac41eed9bb396430f631c35e8d5b27
Dan Mikita <danmikita@gmail.com>
2014-04-28 20:49:12 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,10 +5,8 @@
 #  include soapui
 class soapui {
 
-  $version = '4.5.1'
-
-  package { "SoapUI-${version}":
+  package { "SoapUI-4.6.4":
     provider => 'appdmg',
-    source   => "http://sourceforge.net/projects/soapui/files/soapui/${version}/soapUI-${version}.dmg/download",
-  }
+    source   => "http://downloads.sourceforge.net/project/soapui/soapui/4.6.4/SoapUI-4.6.4.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsoapui%2Ffiles%2Fsoapui%2F4.6.4%2F&ts=1398732331&use_mirror=hivelocity",
+    }
 }
