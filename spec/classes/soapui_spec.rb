9dcc00bf14352211f80b20818190fdd2af4388b1
Dan Mikita <danmikita@gmail.com>
2014-05-14 16:06:22 -0400

    Update soapui_spec.rb

--- spec/classes/soapui_spec.rb
+++ spec/classes/soapui_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'soapui' do
 
   it { should contain_class('soapui') }
-  it { should contain_package("SoapUI").with_provider('compressed_app') }
-  it { should contain_package("SoapUI").with_source("https://dl.dropboxusercontent.com/s/mpbv75h8eujaxp8/SoapUI-4.6.4.zip") }
+  it { should contain_package("SoapUI-4.6.4").with_provider('compressed_app') }
+  it { should contain_package("SoapUI-4.6.4").with_source("https://dl.dropboxusercontent.com/s/mpbv75h8eujaxp8/SoapUI-4.6.4.zip") }
 
 end
