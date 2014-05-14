1ee454b14facef668812d1f9cce46bdeef41d21e
Dan Mikita <danmikita@gmail.com>
2014-05-14 16:05:48 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #  include soapui
 class soapui {
 
-  package { 'SoapUI':
+  package { 'SoapUI-4.6.4':
     provider => 'compressed_app',
     source   => 'https://dl.dropboxusercontent.com/s/mpbv75h8eujaxp8/SoapUI-4.6.4.zip'
     }
